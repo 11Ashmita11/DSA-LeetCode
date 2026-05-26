5be353dd00951355dc8f31fd110b4a6300898c0b
11Ashmita11 <ashmitapandey56@gmail.com>
2026-05-26 21:44:29 +0530

    added new one now

---
+++ Arrays/sorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1])
+            {
+                return true;
+            }
+        }
+            
+                return false;
+            
+        
+    } 
+};
