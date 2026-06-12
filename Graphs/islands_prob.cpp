9b7c548004e88e7286dea4f4afa5a9bdd448d3df
11Ashmita11 <ashmitapandey56@gmail.com>
2026-06-12 19:51:10 +0530

    added new pr

---
+++ Graphs/islands_prob.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void dfs(int r,int c,vector<vector<char>>&grid){
+        int n=grid.size();
+        int m=grid[0].size();
+        if(r<0||c<0||r>=n||c>=m){
+            return;
+        }
+        if(grid[r][c]=='0'){
+            return;
+        }
+        grid[r][c]='0';
+        
+        dfs(r+1,c,grid);
+        dfs(r-1,c,grid);
+        dfs(r,c+1,grid);
+        dfs(r,c-1,grid);
+
+    }
+    int numIslands(vector<vector<char>>& grid) {
+
+        int n=grid.size();
+        int m=grid[0].size();
+
+        int is=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    is++;
+                    dfs(i,j,grid);
+
+
+                }
+            }
+
+        }
+        return is;
+        
+    }
+};
