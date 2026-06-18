a5c4e6fe30aa34597230fa803983ee8125018213
11Ashmita11 <ashmitapandey56@gmail.com>
2026-06-18 19:48:23 +0530

    hehe

---
+++ Arrays/bsearch.cpp
@@ -0,0 +1,30 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        long long low=1;
+        long long high=n;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            int tg=guess(mid);
+            if(tg==0){
+                return mid;
+            }
+            else if(tg==1){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+};
